2e61d5c5ec65bd55b9142fe86a09acd4e028dbcf
Md. Ibn Masud <sohan.cse.pust@gmail.com>
2025-12-27 00:26:06 +0600

    Add project files.

---
+++ ViewData_MVC_App/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ViewData_MVC_App.Models;
+
+namespace ViewData_MVC_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Id"] = 101;
+            ViewData["Name"] = "Laptop";
+            ViewData["Price"] = 75000.00m;
+            ViewData["Quantity"] = 5;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
